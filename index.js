5ca9e1d3bbd2cab6b41a127230fa2865a4c6f25e
Vinicius Oliveira <fukhaos@gmail.com>
2016-04-16 09:03:20 -0300

    criando o index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-index.js
+'use strict'
+const mongoose = require("./config/db");
+
+let express = require('express');
+let bodyParser = require('body-parser');
+
+let app = express();
+
+app.use(bodyParser.urlencoded({extends: true}));
+app.use(bodyParser.json());
+app.use(bodyParser());
+
+app.listen(3000, () =>{
+  console.log("Rodando na porta 3000");
+})
